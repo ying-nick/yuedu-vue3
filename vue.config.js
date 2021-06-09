bb9be9624a9fd80047bb2cd70669ed7cdf7429cf
DM <276061317@qq.com>
2021-06-09 11:50:15 +0800

    121

--- vue.config.js
+++ vue.config.js
@@ -76,6 +76,12 @@ module.exports = {
         secure: false, // 目标服务器地址是否是安全协议
         changeOrigin: true, // 是否修改来源, 为true时会让目标服务器以为是webpack-dev-server发出的请求!服务端和服务端的请求是没有跨域的
         pathRewrite: { "^/lgn": "/" } // 将/api开头的请求地址, /api 改为 /, 即 /api/xx 改为 /xx
+      },
+      "/jp/*": {
+        target: "http://47.106.243.172:8888/book/", // 目标服务器地址
+        secure: false, // 目标服务器地址是否是安全协议
+        changeOrigin: true, // 是否修改来源, 为true时会让目标服务器以为是webpack-dev-server发出的请求!服务端和服务端的请求是没有跨域的
+        pathRewrite: { "^/jp": "/" } // 将/api开头的请求地址, /api 改为 /, 即 /api/xx 改为 /xx
       }
     }
   },
