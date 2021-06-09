8bac9f67ac60643145a1ae844b2783dfa624154f
DM <276061317@qq.com>
2021-06-09 11:28:03 +0800

    追书接口

--- vue.config.js
+++ vue.config.js
@@ -59,11 +59,11 @@ module.exports = {
     hotOnly: false,
     proxy: {
       //凡是以api开头的请求路径都用下面的代理
-      "/api/*": {
-        target: "", // 目标服务器地址
+      "/zs/*": {
+        target: "http://api.zhuishushenqi.com/", // 目标服务器地址
         secure: false, // 目标服务器地址是否是安全协议
         changeOrigin: true, // 是否修改来源, 为true时会让目标服务器以为是webpack-dev-server发出的请求!服务端和服务端的请求是没有跨域的
-        pathRewrite: { "^/api": "/" } // 将/api开头的请求地址, /api 改为 /, 即 /api/xx 改为 /xx
+        pathRewrite: { "^/zs": "/" } // 将/api开头的请求地址, /api 改为 /, 即 /api/xx 改为 /xx
       },
       "/novel/*": {
         target: "http://api.pingcc.cn/", // 目标服务器地址
