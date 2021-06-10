c7281049850ecf5ffe56492c15d8bc447a4f14da
DM <276061317@qq.com>
2021-06-10 11:46:13 +0800

    12312

---
+++ src/tools/api.js
@@ -0,0 +1,1 @@
+export const detailUrl = "/novel/fiction/search/title/斗罗大陆/1/10";
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,24 @@
+
 <template>
   <div>
-    <el-link type="primary">主要链接</el-link>
+    <button @click="btn">跳转</button>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import zgaxios from '@/tools/zgaxios'
+import { detailUrl } from '@/tools/api'
+export default defineComponent({
+  setup() {
+    const btn = async () => {
+      let {
+        data: {
+          data: { data },
+        },
+      } = await zgaxios('GET', detailUrl)
+      console.log(data[0])
+    }
+    return { btn }
+  },
+})
+</script>
