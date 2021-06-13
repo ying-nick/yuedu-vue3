c5a030da4c4e48bb1e9c40ab371cefaadaf19403
DM <276061317@qq.com>
2021-06-13 21:03:33 +0800

    30%

--- src/views/BookDetails.vue
+++ src/views/BookDetails.vue
@@ -14,8 +14,37 @@
             </div>
           </div>
           <div class="right">
-            <div></div>
-            <div></div>
+            <div class="bookright">
+              <div class="booktext">
+                <div class="bookhead">
+                  <h1>{{list.title}}</h1>
+                  <h3>{{list.author}}著</h3>
+                </div>
+                <div class="bookcenter">
+                  <el-tag type="warning">{{list.categoryName}}</el-tag>
+                  <el-divider direction="vertical"></el-divider>
+                  <el-tag type="success">{{staduce(list.update.chapterStatus)}}</el-tag>
+                </div>
+                <div class="bookfoot">
+                  <h4>
+                    {{words(list.word)}}
+                  </h4>
+                  <el-divider direction="vertical"></el-divider>
+                  <h4>
+                    {{list.chapterNum}}
+                    章
+                  </h4>
+                </div>
+              </div>
+              <div class="rightfoot">
+                <div class="bookdetail">
+                  <div class="bookadd" :style="{background:'url('+require('@/images/bookmark1.png')+') no-repeat'}">
+                    <div><span>加入</span><span>书架</span></div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
           </div>
         </div>
         <div class="aside">
@@ -43,9 +72,29 @@ export default defineComponent({
       list: state.bookDetails,
       like: state.bookDetails.recommend,
     })
-
+    const staduce = computed(() => {
+      return (str) => {
+        switch (str) {
+          case 'END':
+            return '完结'
+          case 'SERIALIZE':
+            return '连载中'
+        }
+      }
+    })
+    const words = computed(() => {
+      return (str) => {
+        if (str == '') {
+          return '1万字'
+        } else {
+          return str
+        }
+      }
+    })
     return {
       ...toRefs(states),
+      staduce,
+      words,
     }
   },
 })
@@ -71,7 +120,7 @@ export default defineComponent({
         .left {
           width: 220px;
           height: 380px;
-          background-color: #fff;
+          // background-color: #fff;
           display: flex;
           flex-direction: column;
           align-items: center;
@@ -99,10 +148,82 @@ export default defineComponent({
           }
         }
         .right {
-          margin: 0 20px;
+          margin: 0 10px;
           flex: 1;
           height: 380px;
-          background-color: #fff;
+          // background-color: #fff;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          .bookright {
+            display: flex;
+            flex-direction: column;
+            width: 95%;
+            height: 95%;
+            .booktext {
+              width: 100%;
+              height: 100px;
+              display: flex;
+              flex-direction: column;
+              justify-content: space-around;
+              align-items: flex-start;
+              .bookhead {
+                width: 100%;
+                height: 31px;
+                display: flex;
+                align-items: flex-end;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                -webkit-line-clamp: 1;
+                overflow: hidden;
+                /*! autoprefixer: off */
+                -webkit-box-orient: vertical;
+                // margin-bottom: 20px;
+                h1 {
+                  margin: 0;
+                  height: 100%;
+                }
+                h3 {
+                  margin: 0;
+                  margin-left: 10px;
+                  color: #999;
+                  // height: 100%;
+                }
+              }
+              .bookcenter {
+                width: 100%;
+                display: flex;
+              }
+              .bookfoot {
+                width: 100%;
+                display: flex;
+                padding: 0 10px;
+                h4 {
+                  margin: 0;
+                }
+              }
+            }
+            .rightfoot {
+              flex: 1;
+              .bookdetail {
+                // padding: 20px;
+                margin-top: 10px;
+                width: 100%;
+                border-top: #cccc 1px solid;
+                position: relative;
+                .bookadd {
+                  position: absolute;
+                  top: 0;
+                  right: 0;
+                  color: #fff;
+                  line-height: 14px;
+                  height: 67px;
+                  overflow: hidden;
+                  width: 37px;
+                }
+              }
+            }
+          }
         }
       }
       .aside {
