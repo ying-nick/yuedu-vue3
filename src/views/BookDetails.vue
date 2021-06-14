26f7f3c04c94ad05d6cd435b4bcea135931d5d74
DM <276061317@qq.com>
2021-06-14 15:04:59 +0800

    轮播

--- src/views/BookDetails.vue
+++ src/views/BookDetails.vue
@@ -38,8 +38,12 @@
               </div>
               <div class="rightfoot">
                 <div class="bookdetail">
+                  <div class="synopsis">{{list.desc}}</div>
                   <div class="bookadd" :style="{background:'url('+require('@/images/bookmark1.png')+') no-repeat'}">
-                    <div><span>加入</span><span>书架</span></div>
+                    <div class="add">
+                      <p>加入</p>
+                      <p>书架</p>
+                    </div>
                   </div>
                 </div>
 
@@ -48,8 +52,45 @@
           </div>
         </div>
         <div class="aside">
-          <h2>为你推荐</h2>
+          <div class="recommed">
+            <h2>精彩推荐</h2>
+            <div class="recdal">
+              <el-carousel class="recal">
+                <el-carousel-item v-for="item in like" :key="item" class="recelem">
+
+                  <div class="recimg">
+                    <el-image style="width: 100%; height: 100%;transform:scale(1.2)" :src="'http://pt.yuenov.com:18888'+item.coverImg" fit="cover"></el-image>
+                  </div>
+                  <h2 class="medium">{{ item.title }}</h2>
+                  <el-tag type="warning">{{item.categoryName}}</el-tag>
+                  <div class="rectext">{{item.desc}}</div>
+                  <el-button type="danger" round>书籍详情</el-button>
+                </el-carousel-item>
+              </el-carousel>
+            </div>
+          </div>
+
         </div>
+      </div>
+      <div class="detailfoot">
+        <div class="footmain">
+          <div class="bookupdata">
+            <el-divider content-position="left">最新章节</el-divider>
+            <div class="updtl">
+              <span>{{list.update.chapterName}}</span>
+              <span>更新时间：{{timeHandler(list.update.time)}}</span>
+            </div>
+          </div>
+          <div class="mainbar">
+            <el-divider content-position="left">大家都在看</el-divider>
+            <el-carousel :interval="4000" type="card" height="200px" class="mainsee">
+              <el-carousel-item v-for="item in 6" :key="item">
+                <h3 class="medium">{{ item }}</h3>
+              </el-carousel-item>
+            </el-carousel>
+          </div>
+        </div>
+        <div class="footaside"></div>
       </div>
     </div>
   </div>
@@ -62,6 +103,7 @@ import { ElMessage, ElLoading } from 'element-plus'
 import { useStore } from 'vuex'
 import zgaxios from '@/tools/zgaxios'
 import { searchUrl, searchUrlYnv } from '@/tools/api'
+import { timeHandler } from '@/tools/util'
 
 export default defineComponent({
   setup() {
@@ -71,6 +113,7 @@ export default defineComponent({
     let states = reactive({
       list: state.bookDetails,
       like: state.bookDetails.recommend,
+      loading: true,
     })
     const staduce = computed(() => {
       return (str) => {
@@ -95,6 +138,7 @@ export default defineComponent({
       ...toRefs(states),
       staduce,
       words,
+      timeHandler,
     }
   },
 })
@@ -130,8 +174,9 @@ export default defineComponent({
             height: 70%;
             overflow: hidden;
             img {
-              width: 110%;
-              height: 110%;
+              width: 100%;
+              height: 100%;
+              transform: scale(1.2);
             }
           }
           .btn {
@@ -193,10 +238,12 @@ export default defineComponent({
               .bookcenter {
                 width: 100%;
                 display: flex;
+                align-items: center;
               }
               .bookfoot {
                 width: 100%;
                 display: flex;
+                align-items: center;
                 padding: 0 10px;
                 h4 {
                   margin: 0;
@@ -209,8 +256,20 @@ export default defineComponent({
                 // padding: 20px;
                 margin-top: 10px;
                 width: 100%;
+                // height: 100%;
                 border-top: #cccc 1px solid;
                 position: relative;
+                .synopsis {
+                  width: 80%;
+                  margin-top: 20px;
+                  display: -webkit-box;
+                  overflow: hidden;
+                  text-overflow: ellipsis;
+                  // -webkit-line-clamp: 5;
+                  overflow: hidden;
+                  /*! autoprefixer: off */
+                  -webkit-box-orient: vertical;
+                }
                 .bookadd {
                   position: absolute;
                   top: 0;
@@ -220,6 +279,23 @@ export default defineComponent({
                   height: 67px;
                   overflow: hidden;
                   width: 37px;
+                  display: flex;
+                  justify-content: center;
+                  align-items: flex-start;
+                  .add {
+                    width: 100%;
+                    // height: 70%;
+                    margin-top: 10px;
+                    display: flex;
+                    flex-direction: column;
+                    p {
+                      width: 100%;
+                      margin: 0;
+                      display: flex;
+                      justify-content: center;
+                      align-items: center;
+                    }
+                  }
                 }
               }
             }
@@ -227,10 +303,114 @@ export default defineComponent({
         }
       }
       .aside {
-        width: 200px;
-        h2 {
-          margin: 0;
+        width: 300px;
+        .recommed {
+          width: 100%;
+          height: 380px;
+          display: flex;
+          flex-direction: column;
+          h2 {
+            margin: 0;
+          }
+          .recdal {
+            flex: 1;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            .recal {
+              width: 100%;
+              height: 100%;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              .el-carousel__container {
+                width: 90%;
+                height: 90%;
+
+                .recelem {
+                  // width: 100%;
+                  // height: 100%;
+                  display: flex;
+                  flex-direction: column;
+                  justify-content: space-around;
+                  align-items: center;
+                  .medium {
+                    display: -webkit-box;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    -webkit-line-clamp: 1;
+                    overflow: hidden;
+                    /*! autoprefixer: off */
+                    -webkit-box-orient: vertical;
+                  }
+                  .recimg {
+                    width: 40%;
+                    height: 40%;
+                    overflow: hidden;
+                  }
+                  .rectext {
+                    width: 80%;
+                    display: -webkit-box;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    -webkit-line-clamp: 3;
+                    overflow: hidden;
+                    /*! autoprefixer: off */
+                    -webkit-box-orient: vertical;
+                  }
+                }
+              }
+              ul {
+                display: none;
+              }
+            }
+          }
         }
+      }
+    }
+    .detailfoot {
+      width: 100%;
+      height: 380px;
+      display: flex;
+      justify-content: space-around;
+      .footmain {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        padding: 0;
+        .bookupdata {
+          width: 100%;
+          .el-divider__text {
+            background-color: transparent;
+            font-weight: 600;
+            font-size: 18px;
+          }
+          .updtl {
+            display: flex;
+            justify-content: space-between;
+            span {
+              padding: 20px;
+            }
+          }
+        }
+        .mainbar {
+          .el-divider__text {
+            background-color: transparent;
+            font-weight: 600;
+            font-size: 18px;
+          }
+          .mainsee {
+            width: 100%;
+            overflow: hidden;
+            ul {
+              display: none;
+            }
+          }
+        }
+      }
+      .footaside {
+        width: 300px;
       }
     }
   }
