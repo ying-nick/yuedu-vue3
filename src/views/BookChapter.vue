0905ec0f9860fb62478247e9abc87562a799e4b4
name-xc <1749960239@qq.com>
2021-06-18 14:17:22 +0800

    Revert "1"
    This reverts commit 4d9707546d16d02123b509fc0804015ffc8952cf.

--- src/views/BookChapter.vue
+++ src/views/BookChapter.vue
@@ -43,8 +43,7 @@
       <div class="content" :style="{ background: back.content }">
         <div class="chapterHead">{{ chapterHead }}</div>
         <div class="main">
-          
-            <p
+          <p
             v-for="item in content"
             :key="item"
             class="text"
@@ -118,17 +117,14 @@ export default defineComponent({
       chapterIdList: [state.chapterId]
     });
     let chapterList = ref(state.chapter);
-    let content = ref([]);
-    let chapterHead = ref("暂无内容");   
-    let textList = ref([])
+    let content = ref("");
+    let chapterHead = ref("暂无内容");
     const getText = async () => {
       let result = await zgaxios("POST", getList, text);
-     
+      console.log(result);
 
       if (result.data.result.code == 0) {
-        // result.data.data.list[0].content.replace(/\n/g,"")
         content.value = result.data.data.list[0].content.split("\n");
-      //  content.value= result.data.data.list[0].content.replace(/\n/g,"<br/>")
         chapterHead.value = result.data.data.list[0].name;
         fullscreenLoading.value = false;
       } else {
