fcc508be307414fa1c3a1751c7a96c90123f0e97
xuning <978206624@qq.com>
2021-06-18 21:01:50 +0800

    Merge branch 'HB' of https://gitee.com/ying-nick/yuedu-vue3 into HB
    # Conflicts:
    #	src/views/cartoon.vue

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -19,33 +19,18 @@
             {{item}}
             </span>-->
             <el-tabs v-model="activeName" v-show="seen" @tab-click="getMaleCategoryList">
-              <el-tab-pane
-                :label="item.categoryName"
-                :name="index==0?'first':''"
-                v-for="(item,index) in MaleBookCategories"
-                :key="item"
-              ></el-tab-pane>
+              <el-tab-pane :label="item.categoryName" :name="index==0?'first':''" v-for="(item,index) in MaleBookCategories" :key="item"></el-tab-pane>
             </el-tabs>
             <el-tabs v-model="activeName" v-show="!seen" @tab-click="getFemaleCategoryList">
-              <el-tab-pane
-                :label="item.categoryName"
-                :name="index==0?'first':''"
-                v-for="(item,index) in FemaleBookCategories"
-                :key="item"
-              ></el-tab-pane>
+              <el-tab-pane :label="item.categoryName" :name="index==0?'first':''" v-for="(item,index) in FemaleBookCategories" :key="item"></el-tab-pane>
             </el-tabs>
           </li>
         </ul>
       </div>
       <!-- 表格部分 -->
       <div class="ca-down">
-        <el-table
-          stripe
-          v-loading="loading"
-          :data="bookListTable.slice((currentPage - 1) * pageSize, currentPage*pageSize)"
-          style="width: 100%"
-          @row-click="handleCurrentChange"
-        >
+        <el-table stripe v-loading="loading" :data="bookListTable.slice((currentPage - 1) * pageSize, currentPage*pageSize)" style="width: 100%"
+          @row-click="handleCurrentChange">
           <el-table-column type="index" label="序号" width="60"></el-table-column>
           <el-table-column prop="categoryName" label="类别" width="120"></el-table-column>
           <el-table-column prop="title" label="书名" width="220"></el-table-column>
@@ -53,36 +38,29 @@
           <el-table-column prop="author" label="作者" width="150"></el-table-column>
           <el-table-column prop="word" label="字数" width="120"></el-table-column>
         </el-table>
-        <el-pagination
-          background
-          layout="prev, pager, next"
-          :key="elementui_page_component_key"
-          :total="total"
-          @current-change="toChangePage"
-          :current-page="currentPage"
-          :page-size="pageSize"
-        ></el-pagination>
+        <el-pagination background layout="prev, pager, next" :key="elementui_page_component_key" :total="total" @current-change="toChangePage"
+          :current-page="currentPage" :page-size="pageSize"></el-pagination>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from "vue";
-import zgaxios from "@/tools/zgaxios";
-import { ElMessage } from "element-plus";
-import router from "../router";
-import { useStore } from "vuex";
+import { defineComponent, reactive, toRefs } from 'vue'
+import zgaxios from '@/tools/zgaxios'
+import { ElMessage, ElLoading } from 'element-plus'
+import router from '../router'
+import { useStore } from 'vuex'
 
 export default defineComponent({
-  name: "category",
+  name: 'category',
   components: {},
   setup() {
-    const { getters, dispatch, commit } = useStore();
+    const { getters, dispatch, commit } = useStore()
     const state = reactive({
       bookListTable: [],
-      defaultName: "male",
+      defaultName: 'male',
       loading: true,
-      activeName: "first",
+      activeName: 'first',
       seen: true,
       pageSize: 10,
       currentPage: 1,
@@ -91,22 +69,22 @@ export default defineComponent({
       total: 10,
       elementui_page_component_key: 1,
       MaleBookCategories: [
-        { categoryId: 1, categoryName: "玄幻小说" },
-        { categoryId: 2, categoryName: "修真小说" },
-        { categoryId: 3, categoryName: "都市小说" },
-        { categoryId: 5, categoryName: "网游小说" },
-        { categoryId: 6, categoryName: "科幻灵异" },
-        { categoryId: 7, categoryName: "历史小说" },
-        { categoryId: 9, categoryName: "其他小说" }
+        { categoryId: 1, categoryName: '玄幻小说' },
+        { categoryId: 2, categoryName: '修真小说' },
+        { categoryId: 3, categoryName: '都市小说' },
+        { categoryId: 5, categoryName: '网游小说' },
+        { categoryId: 6, categoryName: '科幻灵异' },
+        { categoryId: 7, categoryName: '历史小说' },
+        { categoryId: 9, categoryName: '其他小说' },
       ],
       FemaleBookCategories: [
-        { categoryId: 3, categoryName: "都市小说" },
-        { categoryId: 6, categoryName: "科幻灵异" },
-        { categoryId: 8, categoryName: "言情小说" },
-        { categoryId: 9, categoryName: "其他小说" },
-        { categoryId: 10, categoryName: "女生小说" }
-      ]
-    });
+        { categoryId: 3, categoryName: '都市小说' },
+        { categoryId: 6, categoryName: '科幻灵异' },
+        { categoryId: 8, categoryName: '言情小说' },
+        { categoryId: 9, categoryName: '其他小说' },
+        { categoryId: 10, categoryName: '女生小说' },
+      ],
+    })
     // 获取男频小说分类列表
     // const getMaleDefaultTable = async () => {
     //   let { data } = await zgaxios(
@@ -130,113 +108,120 @@ export default defineComponent({
 
     // 强制刷新分页组件
     function autoIncrasePageComKey() {
-      state.elementui_page_component_key++;
+      state.elementui_page_component_key++
       // console.log(state.elementui_page_component_key)
     }
 
     // 封装获取第一页小说列表的函数
-    const getBookList = async url => {
-      state.bookListTable = [];
-      let { data } = await zgaxios("GET", url);
+    const getBookList = async (url) => {
+      const loading = ElLoading.service({
+        lock: true,
+        text: 'Loading',
+        spinner: 'el-icon-loading',
+        background: 'rgba(0, 0, 0, 0.7)',
+      })
+      state.bookListTable = []
+      let { data } = await zgaxios('GET', url)
       // console.log(data,"22222222222222222222")
       // let bookListTable :any= [];
       if (data.result.code == 0) {
+        loading.close()
         // bookListTable = [...state.bookListTable,...data.data.list]
         // console.log(1111111)
-        state.bookListTable = data.data.list;
-        state.loading = false;
-        state.total = data.data.total;
+        state.bookListTable = data.data.list
+        state.loading = false
+        state.total = data.data.total
       } else if (data.result.code == 1009) {
-        state.loading = false;
-        ElMessage.error("操作频繁，请10s后再试");
+        loading.close()
+        state.loading = false
+        ElMessage.error('操作频繁，请10s后再试')
+      } else {
+        loading.close()
+        ElMessage.error('无数据')
       }
-    };
+    }
     // 调用函数获取默认列表
     getBookList(
-      "/ynv/app/open/api/book/getCategoryId?pageNum=1&pageSize=10&categoryId=1&channelId=1"
-    );
+      '/ynv/app/open/api/book/getCategoryId?pageNum=1&pageSize=10&categoryId=1&channelId=1'
+    )
 
     // 男女频道切换的点击事件
     const toChoose = (tab, e) => {
-      if (e.target.innerText == "男频") {
-        state.seen = true;
+      if (e.target.innerText == '男频') {
+        state.seen = true
         getBookList(
-          "/ynv/app/open/api/book/getCategoryId?pageNum=1&pageSize=10&categoryId=1&channelId=1"
-        );
-        autoIncrasePageComKey();
+          '/ynv/app/open/api/book/getCategoryId?pageNum=1&pageSize=10&categoryId=1&channelId=1'
+        )
+        autoIncrasePageComKey()
         // console.log(state.bookListTable, 222222222222);
-      } else if (e.target.innerText == "女频") {
+      } else if (e.target.innerText == '女频') {
         // state.currentPage = 1
-        state.seen = false;
+        state.seen = false
         getBookList(
-          "/ynv/app/open/api/book/getCategoryId?pageNum=1&pageSize=10&categoryId=3&channelId=2"
-        );
-        autoIncrasePageComKey();
+          '/ynv/app/open/api/book/getCategoryId?pageNum=1&pageSize=10&categoryId=3&channelId=2'
+        )
+        autoIncrasePageComKey()
       }
-    };
+    }
 
     // 男频小说分类列表的点击事件
     function getMaleCategoryList(event) {
-      autoIncrasePageComKey();
+      autoIncrasePageComKey()
       // console.log(event.index)
-      let bookCategoryM = state.MaleBookCategories.filter(item => {
-        return state.MaleBookCategories.indexOf(item) == event.index;
-      });
+      let bookCategoryM = state.MaleBookCategories.filter((item) => {
+        return state.MaleBookCategories.indexOf(item) == event.index
+      })
       // console.log(bookCategoryM);
-      state.bookCategoryM = bookCategoryM[0].categoryId;
+      state.bookCategoryM = bookCategoryM[0].categoryId
       getBookList(
         `/ynv/app/open/api/book/getCategoryId?pageNum=1&pageSize=10&categoryId=${state.bookCategoryM}&channelId=1`
-      );
+      )
     }
     // 女频小说的分类列表的点击事件
-    const getFemaleCategoryList = e => {
-      autoIncrasePageComKey();
-      let bookCategoryF = state.FemaleBookCategories.filter(item => {
-        return state.FemaleBookCategories.indexOf(item) == e.index;
-      });
+    const getFemaleCategoryList = (e) => {
+      autoIncrasePageComKey()
+      let bookCategoryF = state.FemaleBookCategories.filter((item) => {
+        return state.FemaleBookCategories.indexOf(item) == e.index
+      })
       // console.log(bookCategoryF);
-      state.bookCategoryF = bookCategoryF[0].categoryId;
+      state.bookCategoryF = bookCategoryF[0].categoryId
       getBookList(
         `/ynv/app/open/api/book/getCategoryId?pageNum=1&pageSize=10&categoryId=${state.bookCategoryF}&channelId=2`
-      );
-    };
+      )
+    }
 
     // 分页
-    const toChangePage = e => {
+    const toChangePage = (e) => {
       // console.log(`当前页: ${e}`);
       if (state.seen) {
-        setTimeout(() => {
-          getBookList(
-            `/ynv/app/open/api/book/getCategoryId?pageNum=${e}&pageSize=10&categoryId=${state.bookCategoryM}&channelId=1`
-          );
-        }, 10000);
+        getBookList(
+          `/ynv/app/open/api/book/getCategoryId?pageNum=${e}&pageSize=10&categoryId=${state.bookCategoryM}&channelId=1`
+        )
       } else {
-        setTimeout(() => {
-          getBookList(
-            `/ynv/app/open/api/book/getCategoryId?pageNum=${e}&pageSize=10&categoryId=${state.bookCategoryF}&channelId=2`
-          );
-        }, 10000);
+        getBookList(
+          `/ynv/app/open/api/book/getCategoryId?pageNum=${e}&pageSize=10&categoryId=${state.bookCategoryF}&channelId=2`
+        )
       }
-    };
+    }
 
     // 点击跳转详情页
     const handleCurrentChange = async (row, column) => {
       // console.log(row.bookId, row.title);
       let { data } = await zgaxios(
-        "GET",
+        'GET',
         `/ynv/app/open/api/book/getDetail?bookId=${row.bookId}`
-      );
+      )
       // console.log(data.data, 2222222);
       if (data.result.code == 1009) {
-        state.loading = false;
-        ElMessage.error("操作频繁，请10s后再试");
-        return;
+        state.loading = false
+        ElMessage.error('操作频繁，请10s后再试')
+        return
       }
       data.result.code == 0 &&
         data.data !== undefined &&
-        commit("getBookDetails", data.data);
-      router.push(`/bookdetails/${row.title}`);
-    };
+        commit('getBookDetails', data.data)
+      router.push(`/bookdetails/${row.title}`)
+    }
 
     return {
       ...toRefs(state),
@@ -245,10 +230,10 @@ export default defineComponent({
       toChangePage,
       getFemaleCategoryList,
       handleCurrentChange,
-      autoIncrasePageComKey
-    };
-  }
-});
+      autoIncrasePageComKey,
+    }
+  },
+})
 </script>
 <style lang="less" scoped>
 .fl {
